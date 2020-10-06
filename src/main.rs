469023d49bf5d51a62158c91bad1cd4be420b73d
Nathan Fox <fuchsnj@gmail.com>
2020-10-06 18:32:23 -0400

    get a window to display

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
-use amethyst::{prelude::*, utils::application_root_dir};
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
 
 struct GameState;
 
-impl EmptyState for GameState {
-    fn on_start(&mut self, _: StateData<'_, ()>) {
+impl SimpleState for GameState {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
         println!("Begin!");
     }
 
-    fn on_stop(&mut self, _: StateData<'_, ()>) {
+    fn on_stop(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
         println!("End!");
     }
 
-    fn update(&mut self, _: StateData<'_, ()>) -> EmptyTrans {
-        println!("Hello from Amethyst!");
-        Trans::Quit
+    fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        Trans::None
     }
 }
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
-    let assets_dir = application_root_dir()?.join("assets");
-    let mut game = Application::new(assets_dir, GameState, ())?;
+
+    let app_root = application_root_dir()?;
+    let assets_dir = app_root.join("assets");
+    let display_config_path = app_root.join("config/display.ron");
+
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let mut game = Application::new(assets_dir, GameState, game_data)?;
+
     game.run();
 
     Ok(())

42825de7789062bdb44ceece42af789d6d387676
Nathan Fox <fuchsnj@gmail.com>
2020-10-10 23:53:53 -0400

    add basic Bevy example with moveable ball

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,81 @@
-mod systems;
 mod components;
 
-use amethyst::{
-    prelude::*,
-    renderer::{
-        plugins::{RenderShaded3D, RenderToWindow},
-        types::DefaultBackend,
-        RenderingBundle,
-    },
-    utils::application_root_dir,
-};
-use amethyst::assets::{PrefabLoader, RonFormat, PrefabLoaderSystemDesc};
-use amethyst::utils::scene::BasicScenePrefab;
-use amethyst::renderer::rendy::mesh::{Position, Normal, TexCoord};
-use amethyst::core::TransformBundle;
-use amethyst::input::{InputBundle, StringBindings};
+use bevy::prelude::*;
 use crate::components::Character;
-use crate::systems::MovementSystem;
 
-
-type MyPrefabData = BasicScenePrefab<(Vec<Position>, Vec<Normal>, Vec<TexCoord>)>;
-
-struct GameState;
-
-impl SimpleState for GameState {
-    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
-        println!("Loading...");
-        let character = data.world.exec(|loader: PrefabLoader<'_, MyPrefabData>| {
-            loader.load("prefab/square_character.ron", RonFormat, ())
-        });
-        let stage = data.world.exec(|loader: PrefabLoader<'_, MyPrefabData>| {
-            loader.load("prefab/stage.ron", RonFormat, ())
-        });
-        data.world.register::<Character>();
-        data.world.create_entity().with(character).with(Character::new()).build();
-        data.world.create_entity().with(stage).build();
-        println!("Ready!");
-    }
-
-    fn on_stop(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
-        println!("End!");
-    }
-
-    fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
-        Trans::None
+fn character_movement_system(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Character, &mut Transform)>,
+) {
+    for (character, mut transform) in &mut query.iter() {
+        let translation = transform.translation_mut();
+        let move_speed = 5.0;
+        if keyboard_input.pressed(KeyCode::D) {
+            *translation.x_mut() += time.delta_seconds * move_speed;
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            *translation.x_mut() -= time.delta_seconds * move_speed;
+        }
+        if keyboard_input.pressed(KeyCode::W) {
+            *translation.y_mut() += time.delta_seconds * move_speed;
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            *translation.y_mut() -= time.delta_seconds * move_speed;
+        }
     }
 }
 
-fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
+fn main() {
+    App::build()
+        .add_resource(Msaa { samples: 4 })
+        .add_default_plugins()
+        .add_startup_system(setup.system())
+        .add_system(character_movement_system.system())
+        .run();
+}
 
-    let app_root = application_root_dir()?;
-    let assets_dir = app_root.join("assets");
-    let display_config_path = app_root.join("config/display.ron");
-    let binding_path = app_root.join("config").join("bindings.ron");
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        // The ground
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+            material: materials.add(Color::rgb(0.1, 0.2, 0.1).into()),
+            // transform: Transform::from_non_uniform_scale(Vec3::new(2.0, 1.0, 1.0 / 10.0)),
+            ..Default::default()
+        })
 
-    let input_bundle = InputBundle::<StringBindings>::new()
-        .with_bindings_from_file(binding_path)?;
+        // light
+        .spawn(LightComponents {
+            transform: Transform::from_translation(Vec3::new(-4.0, 8.0, 8.0)),
+            ..Default::default()
+        })
 
-    let game_data = GameDataBuilder::default()
-        .with_system_desc(PrefabLoaderSystemDesc::<MyPrefabData>::default(), "", &[])
-        .with_bundle(TransformBundle::new())?
-        .with_bundle(input_bundle)?
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)?
-                        .with_clear([0.0, 0.0, 0.0, 1.0]),
-                )
-                .with_plugin(RenderShaded3D::default()),
-        )?
-        .with(MovementSystem, "movement_system", &["input_system"]);
+        // camera
+        .spawn(Camera3dComponents {
+            transform: Transform::new(Mat4::face_toward(
+                Vec3::new(0.0, 4.0, 12.0),
+                Vec3::new(0.0, 3.0, 0.0),
+                Vec3::new(0.0, 1.0, 0.0),
+            )),
+            ..Default::default()
+        });
 
-    let mut game = Application::new(assets_dir, GameState, game_data)?;
+    let character_model = PbrComponents {
+        mesh: meshes.add(Mesh::from(shape::Icosphere {
+            subdivisions: 4,
+            radius: 0.5,
+        })),
+        material: materials.add(Color::rgb(0.1, 0.4, 0.8).into()),
+        transform: Transform::from_translation(Vec3::new(1.5, 1.5, 1.5)),
+        ..Default::default()
+    };
 
-    game.run();
-
-    Ok(())
+    commands.spawn(character_model)
+        .with(Character::new());
 }

02f82ace5a4ecc6769a3fffd833e5275376b664c
Robert Benson <derelbenkoenig@gmail.com>
2020-10-31 17:40:59 -0400

    Bevy (#15)
    Convert from Amethyst to Bevy
    Co-authored-by: Nathan Fox <fuchsnj@gmail.com>

--- src/components/character.rs
+++ src/components/character.rs
@@ -1,4 +1,3 @@
-use amethyst::core::ecs::{Component, DenseVecStorage};
 
 pub struct Character {
     /* Doesn't contain data yet, this is just a marker */
@@ -8,8 +7,4 @@ impl Character {
     pub fn new() -> Character {
         Character {}
     }
-}
-
-impl Component for Character {
-    type Storage = DenseVecStorage<Self>;
 }
